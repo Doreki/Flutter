7ddd1e534dbb09b200559d5f75f984418ec75e96
Doreki <idh0325@naver.com>
2023-05-05 12:28:15 +0900

    row and column

--- hello_flutter/lib/main.dart
+++ hello_flutter/lib/main.dart
@@ -1,25 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/screen/home_screen.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner:false,home: HomeScreen()));
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
 }
 
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFF99231),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'asset/img/logo.png',
-          ),
-          CircularProgressIndicator(
-            color: Colors.white,
-          ),
-        ],
-      ),
-    );
-  }
-}
