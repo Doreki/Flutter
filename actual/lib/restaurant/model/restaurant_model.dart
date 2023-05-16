9f0e394a4b1744468c513d777fb09f3f2267fb3d
Doreki <idh0325@naver.com>
2023-05-16 17:38:53 +0900

    레트로 핏

--- actual/lib/restaurant/model/restaurant_model.dart
+++ actual/lib/restaurant/model/restaurant_model.dart
@@ -1,10 +1,23 @@
+import 'package:actual/common/utils/data_utils.dart';
+import 'package:json_annotation/json_annotation.dart';
+
 import '../../common/const/data.dart';
 
-enum RestaurantPriceRange { expensive, medium, cheap }
+part 'restaurant_model.g.dart';
 
+enum RestaurantPriceRange {
+  expensive,
+  medium,
+  cheap,
+}
+
+@JsonSerializable()
 class RestaurantModel {
   final String id;
   final String name;
+  @JsonKey(
+    fromJson: DataUtils.pathToUrl,
+  )
   final String thumbUrl;
   final List<String> tags;
   final RestaurantPriceRange priceRange;
@@ -12,6 +25,9 @@ class RestaurantModel {
   final int ratingsCount;
   final int deliveryTime;
   final int deliveryFee;
+
+  factory RestaurantModel.fromJson(Map<String, dynamic> json) =>
+      _$RestaurantModelFromJson(json);
 
   RestaurantModel(
       {required this.id,
@@ -24,21 +40,24 @@ class RestaurantModel {
       required this.deliveryTime,
       required this.deliveryFee});
 
-  factory RestaurantModel.fromJson({
-    required Map<String, dynamic> json,
-  }) {
-    return RestaurantModel(
-      id: json['id'],
-      name: json['name'],
-      thumbUrl: 'http://$ip${json['thumbUrl']}',
-      tags: List<String>.from(json['tags']),
-      priceRange: RestaurantPriceRange.values.firstWhere(
-        (e) => e.name == json['priceRange'],
-      ),
-      ratings: json['ratings'],
-      ratingsCount: json['ratingsCount'],
-      deliveryTime: json['deliveryTime'],
-      deliveryFee: json['deliveryFee'],
-    );
-  }
+  Map<String, dynamic> toJson() => _$RestaurantModelToJson(this);
+
+
+// factory RestaurantModel.fromJson({
+//   required Map<String, dynamic> json,
+// }) {
+//   return RestaurantModel(
+//     id: json['id'],
+//     name: json['name'],
+//     thumbUrl: 'http://$ip${json['thumbUrl']}',
+//     tags: List<String>.from(json['tags']),
+//     priceRange: RestaurantPriceRange.values.firstWhere(
+//       (e) => e.name == json['priceRange'],
+//     ),
+//     ratings: json['ratings'],
+//     ratingsCount: json['ratingsCount'],
+//     deliveryTime: json['deliveryTime'],
+//     deliveryFee: json['deliveryFee'],
+//   );
+// }
 }
